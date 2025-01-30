cfdadfe418cbe185949fa69d21236d0cab40cfc4
kyoumaru <166464753+kyoumaru@users.noreply.github.com>
2025-01-30 17:40:49 +0800

    Oh god we figured out how branches work kinda. But yea this is just the file with only the gui function working

--- Fraction.py
+++ Fraction.py
@@ -7,14 +7,6 @@ class Fraction(object):
 
     def gcd(a, b):
         #TODO
-        if (a == 0 or b == 0):
-            return 0
-        elif (a == b):
-            return a
-        else:
-            if (a > b):
-                return Fraction.gcd(b, a-b)
-            return Fraction.gcd(b, b-a)
         pass
 
     def get_numerator(self):
